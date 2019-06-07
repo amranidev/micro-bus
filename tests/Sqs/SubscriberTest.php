74bff8de9ae4df47800203e99035bc7248490f83
Amrani Houssain <amranidev@gmail.com>
2019-06-07 00:09:01 +0000

    Update SubscriberTest.php

--- tests/Sqs/SubscriberTest.php
+++ tests/Sqs/SubscriberTest.php
@@ -4,6 +4,7 @@ namespace Amranidev\MicroBus\Tests\Sqs;
 
 use Amranidev\MicroBus\Tests\TestCase;
 use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Contracts\Container\Container;
 use Amranidev\MicroBus\Tests\Sqs\Traits\InteractsWithSqs;
 
 class SubscriberTest extends TestCase
@@ -13,7 +14,7 @@ class SubscriberTest extends TestCase
     /**
      * The job handler instance.
      *
-     * @var mixed
+     * @var \Illuminate\Queue\Jobs\Job
      */
     public $job;
 
@@ -24,38 +25,76 @@ class SubscriberTest extends TestCase
     {
         parent::setUp();
 
-        $this->sendMessage(file_get_contents(__DIR__ . '/messages/message.json'));
     }
 
     /** @test */
     public function work_incoming_messages_published_by_sns()
     {
-        /** @var \Illuminate\Config\Repository $config */
-        $config = $this->app['config'];
+        $this->sendMessage(file_get_contents(__DIR__ . '/messages/message.json'));
 
+        // Listen for the job to be processed.
         $this->app['events']->listen(JobProcessed::class, function ($event) {
-            $this->job = json_decode($event->job->getInstance()->payload);
+            $this->job = $event->job;
         });
 
+        // Run work command.
         $this->artisan('queue:work', ['--once' => true]);
+
+        // Get the underlying subscriber job (JobHandler).
+        $subscriber = $this->job->getInstance();
+
+        // Command job should not be null.
+        self::assertNotNull($subscriber);
+
+        // Decode the payload.
+        $payload = json_decode($subscriber->payload);
 
         self::assertEquals(
             'Hooray! you have a message!',
-            $this->job->body
+            $payload->body
         );
+
+        // The subscriber job should be an instance of JobHandler.
+        self::assertInstanceOf(JobHandler::class, $subscriber);
+
+        // JobHandler@handle should instantiate Container using IOC.
+        self::assertInstanceOf(Container::class, $subscriber->container);
+
+        // Job should be deleted
+        self::assertTrue($this->job->isDeleted());
     }
 
     /** @test */
-    public function work_only_subscribed_jobs()
+    public function ignore_other_messages_with_different_topic_arn()
     {
         /** @var \Illuminate\Config\Repository $config */
         $config = $this->app['config'];
 
+        $this->sendMessage(file_get_contents(__DIR__ . '/messages/message.json'));
+
+        // Listen for the job to be processed.
         $this->app['events']->listen(JobProcessed::class, function ($event) {
-            $this->job = $event->job->getInstance();
+            $this->job = $event->job;
         });
 
-        $config->set('subscriber.subscribers.'. JobHandler::class, 'arn:aws:sns:eu-west-1:s1111111111:my-topic-arn2');
+        // Set local subscriber class.
+        $config->set('subscriber.subscribers.' . JobHandler::class, 'arn:aws:sns:eu-west-1:s1111111111:my-topic-arn2');
+        $this->artisan('queue:work', ['--once' => true]);
+
+        // The worker should ignore the message and the job should be null.
+        self::assertNull($this->job);
+    }
+
+    /** @test */
+    public function ignore_messages_with_no_topic_arn()
+    {
+        // Listen for the job to be processed.
+        $this->app['events']->listen(JobProcessed::class, function ($event) {
+            $this->job = $event->job;
+        });
+
+        $this->sendMessage(file_get_contents(__DIR__ . '/messages/without_topic_arn.json'));
+
         $this->artisan('queue:work', ['--once' => true]);
 
         self::assertNull($this->job);
