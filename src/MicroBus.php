fb438aee35c929e78eb2be6a245a0608dae880f7
Amrani Houssain <amranidev@gmail.com>
2019-11-09 15:25:45 +0100

    Create MicroBus class

---
+++ src/MicroBus.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Amranidev\MicroBus;
+
+/**
+ * Class MicroBus
+ *
+ * @package Amranidev\MicroBus
+ */
+class MicroBus
+{
+    /**
+     * The Cashier library version.
+     *
+     * @var string
+     */
+    const VERSION = '0.1.5';
+}
