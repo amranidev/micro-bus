fb478f0d95eacb3ded7026f1c1c62f160c8dd38a
amrani <amranidev@gmail.com>
2021-05-02 00:10:00 +0000

    Update PublisherFifo.php

--- src/Sns/PublisherFifo.php
+++ src/Sns/PublisherFifo.php
@@ -6,6 +6,15 @@ use Ramsey\Uuid\Uuid;
 
 class PublisherFifo extends BaseSns
 {
+    /**
+     * Publish Message to SNS fifo
+     *
+     * @param $topic
+     * @param $message
+     * @param array $payload
+     * @return \Aws\Result
+     * @throws \Exception
+     */
     public function publish($topic, $message, array $payload = [])
     {
         $topic = $this->getTopicArn($topic);
