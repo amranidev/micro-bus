ed747dd7bf78377bdac2d8017a55b2652e4d0458
Houssain Amrani <amranidev@users.noreply.github.com>
2022-12-23 17:55:54 +0100

    chore: remove serialize from subscriber

--- src/Sqs/SqsJob.php
+++ src/Sqs/SqsJob.php
@@ -45,26 +45,9 @@ class SqsJob extends AbstractSqsJob
         $payload = parent::payload();
 
         $payload['job'] = $this->handler;
-
-        if ($this->isJson($payload['Message']) === true) {
-            $payload['data'] = $payload['Message'];
-        } else {
-            $payload['data'] = unserialize($payload['Message']);
-        }
+        $payload['data'] = $payload['Message'];
 
         return $payload;
-    }
-
-    /**
-     * @param $message
-     *
-     * @return bool
-     */
-    private function isJson($message)
-    {
-        $result = json_decode(trim($message, '"'), true);
-
-        return is_array($result);
     }
 
     /**
